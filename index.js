344c5ce3242bccf5f11872484295b441b1a0ca93
MayanthaS <sapumalmayantha@gmail.com>
2025-10-20 01:06:04 +0530

    learning primitive data types

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let name = "change  sanoj";
 console.log("Hello, " + name);
+//primitive types in typescript
+let myname = "Mayantha Sapumal";
+let age = 22;
+let isStudent = true;
+let myundefined = undefined;
+let mynull = null;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 let name: string = "change  sanoj";
 console.log("Hello, " + name);
+
+//primitive types in typescript
+let myname: string = "Mayantha Sapumal";
+let age: number = 22;
+let isStudent: boolean = true;
+
+let myundefined: undefined = undefined;
+let mynull: null = null;
